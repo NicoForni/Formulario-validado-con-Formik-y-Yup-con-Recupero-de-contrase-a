a719bb8e5540bef18e3dc70d98fbcd2c15d479ad
nico <nico.forni@hotmail.com>
2022-03-15 15:35:50 -0300

    ads

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,9 +1,11 @@
 import "./Form.css";
-import React from "react";
+import React, { useState } from "react";
 import {Formik} from "formik";
 
 const Form = () => {
-    
+    const [password, setPassword] = useState(false)
+
+ 
 
     return (
         <>
@@ -13,11 +15,22 @@ const Form = () => {
                     <h2>Log in</h2>
                         <div className="usuario">
                             <label htmlFor="name">Usuario <div style={{color: "red", display:"contents"}}> *</div></label>
-                            <input className="input-usuario" type="text" id="name" name="name" minLength={6} placeholder="Ingresá tu nombre de usuario"/>
+                            <input className="input-usuario" 
+                            type="text" 
+                            id="name" 
+                            name="name" 
+                            minLength={6}
+                            placeholder="Ingresá tu nombre de usuario"/>
                         </div>
                         <div className="password">
                             <label htmlFor="password">Contraseña <div style={{color: "red", display:"contents"}}> *</div></label>
-                            <input className="input-password" type="password" id="password" name="password" placeholder="Ingresá tu contraseña"/>
+                            <input className="input-password" 
+                            type={password ? "password" : "text"}
+                            id="password" 
+                            name="password" 
+                            placeholder="Ingresá tu contraseña"                                                                                    
+                            />
+                            <span><img className="show-hide" src={password ? "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQGmVba5IO7cb6dEShqSmQg30K4_fPiV61RNYHJsnBbRh6aNkhZ_H2YfRAFAqXm_amZ6-A&usqp=CAU" : "https://www.pngfind.com/pngs/m/59-593921_png-file-svg-password-eye-icon-png-transparent.png"} alt="img" onClick={() => setPassword(!password)}></img></span>                            
                         </div>
                         <div className="forgot-password">¿Olvidaste tu contraseña?</div>
                     <button type="submit" className="boton-ingresar">Ingresar</button>
